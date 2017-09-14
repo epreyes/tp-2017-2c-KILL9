862539a70687a938b7faee432dad082eb3c883e7
cbaltazar <cbaltazar@est.frba.utn.edu.ar>
2017-09-14 12:06:53 -0300

    fix transform

--- master/src/master.c
+++ master/src/master.c
@@ -14,110 +14,73 @@
 #include <string.h>
 #include "answers.h"
 
-int ejecutarWorker(){
-	usleep(1000);
-	printf("worker ejecutado");
-	return EXIT_SUCCESS;;
-}
-/*
-int ejecutarHilos() {
-	pthread_t threads[3];
-	void * retvals[3];
-	int count,i;
-	for(count = 0; count < 3; ++count){
-		if(pthread_create(&threads[count],NULL,(void*)ejecutarWorker,NULL)!=0){
-			printf("error: no se pudo iniciar el nodo %d\n", count);
-			break;
-		};
-		printf("worker %d ejecutando\n", count);
-	};
-	printf("Ejecutando ");
-	for(i = 0; i < count; ++i){
-		if(pthread_join(threads[i], &retvals[i])!=0){
-			printf("error: no se pudo joinear el hilo del nodo %d\n", count);
-		}else
-			printf("Hilo del nodo %d joineado con exito \n", i);
-	}
-	puts("procesamiento de workers finalizado...");
-	return EXIT_SUCCESS;
-}
-*/
+typedef struct block{
+		int pos;
+		int size;
+	}block;
+
 void getFileInfo(char* fileName){
 	printf("hola soy %s \n",fileName);
 }
 
 void validateArgs(int argc, char* argv[]){
-	int i;
+	int count;
 	if(argc!=5){
 			printf("\x1b[31m" "> Número de argumentos inválido! \n" "\x1b[0m");
 			exit(0);
 	};
-	for(i=3;i<=4;++i){
-		if(strncmp(argv[i],"yamafs:/",8)!=0){
-			printf("\x1b[31m" "El archivo %s debe pertenecer a yamafs, no olvide el prefijo yamafs:\n" "\x1b[0m",argv[i]);
+	for(count=3;count<=4;++count){
+		if(strncmp(argv[count],"yamafs:/",8)!=0){
+			printf("\x1b[31m" "El archivo %s debe pertenecer a yamafs, no olvide el prefijo yamafs:\n" "\x1b[0m",argv[count]);
 			exit(0);
 		}
 	}
 };
 
-int prepararConexion(tr_datos respuesta[]){
-	int i=0;
-	int j;
-	int nodo, cantRegistros;
-	int cantNodos;
-	struct bloque{
-		int posicion;
-		int tamanio;
-	};;
-	struct bloque bloques[256];
-	//-----------------
-		pthread_t *threads;
-		void * retvals;
-
-	//-----------------
-
-	cantRegistros=sizeof(respuesta);
-
-	while(i<=cantRegistros){
-		cantNodos=0;
-		nodo = respuesta[i].nodo;
-		j=0;
-		while(respuesta[i].nodo==nodo && i<=cantRegistros){
-			bloques[j].posicion = respuesta[i].bloque;
-			bloques[j].tamanio = respuesta[i].tamanio;
-			i++; j++;
-		};
-		//Inicio Hilo
-		if(pthread_create(&threads[cantNodos],NULL,(void*)ejecutarWorker,NULL)!=0){
-			printf("error: no se pudo iniciar el nodo %d\n",cantNodos);
-			break;
-		};
-		cantNodos++;
+int runWorkerThread(int node, block* blocks, int quantity, char* server){
+	int i;
+	printf("estableciendo conexion con nodo %d del server: %s \n", node, server);
+	usleep(1000000);
+	for (i = 0; i <= quantity; ++i) {
+		printf("\t posicion: %d\n", blocks[i].pos);
+		printf("\t tamanio: %d\n", blocks[i].size);
 	};
+	return EXIT_SUCCESS;
+}
 
-	for(i = 0; i < cantNodos; ++i){
-		if(pthread_join(threads[i], &retvals[i])!=0){
-			printf("error: no se pudo joinear el hilo del nodo %d\n", cantNodos);
-		}else
-			printf("Hilo del nodo %d joineado con exito \n", i);
-	}
+/////////////////////////TRANSFORM///////////////////////////////////
+
+int transformFile(tr_datos yamaAnswer[], int totalRecords){
+	int blockCounter=0,recordCounter=0, nodo;
+	block *blocks=NULL;
+	blocks=malloc(sizeof(block));
+
+	while(recordCounter<totalRecords){
+		nodo = yamaAnswer[recordCounter].nodo;	//init first key
+		while(yamaAnswer[recordCounter].nodo==nodo && recordCounter<=totalRecords){
+			blocks=realloc(blocks,sizeof(block)*(blockCounter+1));
+			blocks[blockCounter].pos = yamaAnswer[recordCounter].bloque;
+			blocks[blockCounter].size = yamaAnswer[recordCounter].tamanio;
+			blockCounter++;
+			recordCounter++;
+		};
+		runWorkerThread(nodo, blocks, blockCounter-1, yamaAnswer[nodo].direccion); //crea hilo, se conecta, pasa datos y espera respuesta
+		blockCounter = 0;
+	};
+	free(blocks);
 	return EXIT_SUCCESS;
 };
 
-void transformar(char* program){
-	//solicitarDatos(); 				//conecta por socket y obtiene los datos de YAMA
-	prepararConexion(tr_answer);		//ordena los datos para pedir transform
-	//ejecutarHilos();
-}
 
 ///////////MAIN PROGRAM///////////
 
 int main(int argc, char* argv[]){
+	int answerSize = sizeof(tr_answer)/sizeof(tr_answer[0]);
+
 	//validateArgs(argc, argv);					//valido argumentos
-	transformar(argv[1]);					//se conecta a YAMA y devuelve array de struct
+	transformFile(tr_answer,answerSize);					//se conecta a YAMA y devuelve array de struct
 	//runLocalReduction(argv[2]);				//ordena ejecución de Reductor Local
 	//runGlobalReduction						//ordena ejecución de Reductor Global
 	//saveResult(argv[])						//ordena guardado en FileSystem
 	return EXIT_SUCCESS;
 }
-
