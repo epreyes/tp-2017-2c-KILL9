f40e0ba7ec8f7702f8df32ab5c554e33781a73f7
miguel-t <tomicha.miguel@gmail.com>
2017-09-14 09:28:53 -0300

    agrego protocolo datanode -> filesystem

---
+++ tp-share-library/connections/handler.h
@@ -0,0 +1,16 @@
+/*
+ * handler.h
+ *
+ *  Created on: 14/9/2017
+ *      Author: utnso
+ */
+
+#ifndef CONNECTIONS_HANDLER_H_
+#define CONNECTIONS_HANDLER_H_
+
+
+
+
+int libtp_sendMessage(int, const void*, size_t, t_log*);
+
+#endif /* CONNECTIONS_HANDLER_H_ */
---
+++ tp-share-library/protocolos/protocols.h
@@ -0,0 +1,17 @@
+/*
+ * protocols.h
+ *
+ *  Created on: 14/9/2017
+ *      Author: miguel tomicha
+ */
+
+#ifndef PROTOCOLOS_PROTOCOLS_H_
+#define PROTOCOLOS_PROTOCOLS_H_
+
+/**Definir los distintos  tipos de mensajes que envia cada proceso**/
+//
+typedef enum PROTOCOLO_DATANODE_TO_FILESYSTEM{
+	HANDSHAKE_CONNECT_DATANODE_TO_FILESYSTEM
+} PROTOCOLO_DATANODE_TO_FILESYSTEM;
+
+#endif /* PROTOCOLOS_PROTOCOLS_H_ */
