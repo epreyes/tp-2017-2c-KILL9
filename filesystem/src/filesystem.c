5009050b93fab57120e6c4d5a9653d94c0079f2b
miguel-t <tomicha.miguel@gmail.com>
2017-09-26 09:28:18 -0300

    prueba get bloque

--- filesystem/src/filesystem.c
+++ filesystem/src/filesystem.c
@@ -8,45 +8,89 @@
  ============================================================================
  */
 
+#include <commons/config.h>
 #include <connections/socket.h>
-#include <stdio.h>
+#include <connections/data_node_prot.h>
+#include <protocol/protocol.h>
+#include <pthread.h>
 #include <stdlib.h>
+#include <struct/struct.h>
+
 #include "header/fileSystem.h"
+
 int main(void) {
 
 	load_file_system_properties();
-	//Mock fileSysten socket servidor
-	// socket thread
-	int * new_sock;
-	listenning_socket = open_socket(file_system_config->file_system_puerto,	SOCKET_BACKLOG);
-	for (;;) {
-		new_sock = malloc(1);
-		* new_sock = accept_connection(listenning_socket);
-		//Procesamos las peticiones
-		process_request(new_sock);
-	}
-	close_socket(listenning_socket);
+
+	loadServidorMultihread();
 
 	return EXIT_SUCCESS;
 }
 
+void loadServidorMultihread(){
+	t_paquete* paquete;
+	int socket_fd;
+	int  new_connection = open_socket("localhost",file_system_config->file_system_port);
+	listen(new_connection,1024);
 
+	while(1){
+		socket_fd = accept_connection(new_connection);
 
+		paquete = recibirPaquete(socket_fd);
 
-int process_request(int socket){
-	PROTOCOL_DATANODE_TO_FILESYSTEM typeReceiveMessage;
+		switch (paquete->idProceso) {
+			case DATANODE:
+				createThread(socket_fd);
+				break;
+			default	:
+				break;
+		}
 
-	libtp_receiveMessage(socket, &tipoMensajeRecibido,
-			sizeof(PROTOCOLO_KERNEL_A_FS), infoLogger);
-	//TODO manejar peticiones
+	}
+		close_socket(new_connection);
 }
 
+void createThread(int socket_fd){
+	pthread_attr_t attr;
+	pthread_t thread;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	pthread_create(&thread, &attr, &process_request, (void *) &socket_fd);
+	pthread_attr_destroy(&attr);
+}
+
+int process_request(void * socket_fd){
+	int fd_datanode = *(int*) socket_fd;
+	t_paquete* paquete;
+	t_dn_get_block_resp * resp;
+
+	log_info(infoLogger, "SE ACEPTO DATANODE A FD [%d] \n", fd_datanode);
+
+	enviarDatos(fd_datanode, DATANODE, ACCESOAPROBADO, 2 * sizeof(int), &fd_datanode);
+	resp = dn_get_block(fd_datanode,0,infoLogger);
+
+
+	printf("-- Inicio Informacion recibida de dataNode");
+
+	void * buffer = resp ->buffer;
+	int i;
+	for (i = 0; i < 1076; i++) {
+		char c;
+		c = ((char *) buffer)[i];
+		putchar(c);
+	}
+	printf("-- Fin Informacion recibida de dataNode ");
+}
 
 //TODO mover a otro archivo de funciones
-void  load_data_node_properties_files(){
-	config = config_create("./properties/filSystem.properties");
+void load_file_system_properties(){
+	config = config_create("./properties/fileSystem.properties");
 	file_system_config = malloc(sizeof(t_file_system_config));
-	file_system_config ->file_system_puerto = config_get_string_value(config,"IP_FILESYSTEM");
-	file_system_config ->file_system_puerto = config_get_string_value(config,"PORT_FILESYSTEM");
+	file_system_config->file_sytem_ip= config_get_string_value(config,"IP_FILESYSTEM");
+	file_system_config->file_system_port = config_get_string_value(config,"PORT_FILESYSTEM");
 	free(config);
+
+	/**creacion de log */
+	infoLogger = log_create("fileSystem.log","FileSystem",0,LOG_LEVEL_INFO);
+	errorLogger = log_create("fileSystemError.log","FileSystem",0,LOG_LEVEL_ERROR);
 }
--- filesystem/src/header/fileSystem.h
+++ filesystem/src/header/fileSystem.h
@@ -12,15 +12,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <commons/config.h>
-#include <struct/struct.h>
+#include <protocol/protocol.h>
+#include <commons/log.h>
+#include <pthread.h>
+
+#define DATANODE 1
+#define MEMORIA 2
 #define	SOCKET_BACKLOG   50
 
-/** Variables globales **/
+/***********************************************
+ ************ Variables globales ***************
+ ***********************************************/
 t_config * config;
 t_file_system_config* file_system_config;
+
+/** log **/
+t_log* infoLogger;
+t_log* errorLogger;
+
 int listenning_socket;
+uint32_t vg_tamanio_memoria, vg_tamanio_marco, vg_cant_paginas_adm,
+		vg_cant_marcos;
+int vg_paginas_stack;
+
+typedef enum {
+	ACCESOAPROBADO = 0, ACCESODENEGADO = 1
+} handshake;
+
+typedef enum {
+	LEER = 1, ESCRIBIR = 2
+} tipoCpu;
+
 
 /** Funciones **/
 void load_file_system_properties(void);
+
+int process_request(void * socket);
+
+void loadServidorMultihread(void);
+
+void process_dataNode();
 
 #endif /* HEADER_FILESYSTEM_H_ */
