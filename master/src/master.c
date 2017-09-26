f781ae1598c5d4918247760d1fa688c9a5510300
cbaltazar <cbaltazar@est.frba.utn.edu.ar>
2017-09-26 18:50:55 -0300

    fix concurrencia threads

--- master/src/master.c
+++ master/src/master.c
@@ -13,8 +13,7 @@
 #include <pthread.h>
 #include <string.h>
 #include "answers.h"
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+#include "master.h"
 
 typedef struct block{
 	int pos;
@@ -44,16 +43,13 @@ void validateArgs(int argc, char* argv[]){
 
 void *runWorkerThread(void* data){
 	int i;
-	dataThread* datos = data;
-
-	pthread_mutex_lock(&mutex);
+	dataThread* datos = (dataThread*) data;
 	printf("hilo iniciado:%d\n servidor:%s\n",datos[0].node,datos[0].conector);
-
-	for (i = 0; i <= (datos[0].blocksCount); ++i)
+	for (i = 0; i <= (datos[0].blocksCount); ++i){
 		printf("\t nodo:%d \t pos:%d  \t tam:%d\n", datos[0].node, datos[0].blocks[i].pos, datos[0].blocks[i].size);
-	pthread_mutex_unlock(&mutex);
-
-	return EXIT_SUCCESS;
+	}
+	//conectarse con WORKER y esperar respuesta.
+	return NULL;
 }
 
 ////////////////////////////////////TRANSFORM/////////////////////////////////////////
@@ -84,34 +80,23 @@ int transformFile(tr_datos yamaAnswer[], int totalRecords){
 		dataThreads[nodeCounter].node=nodo;
 		strcpy(dataThreads[nodeCounter].conector,yamaAnswer[recordCounter-1].direccion);
 		dataThreads[nodeCounter].blocksCount=blockCounter-1;
-
-
-		//creo el hilo y paso params
-		pthread_create(&threads[nodeCounter],NULL,(void*) runWorkerThread, (void*) &dataThreads[nodeCounter]);
 		nodeCounter++;
 		blockCounter = 0;
-		usleep(99999);
 	}
 
-//----TEST-------
-/*
-	int i,j;
-	for (i = 0; i < nodeCounter; ++i){
-		printf("nodo:%d\t servidor:%s\n",dataThreads[i].node,dataThreads[i].conector);
-		for (j = 0; j <= (dataThreads[i].blocksCount); ++j)
-			printf("\t bloque:%d\t size:%d\n",dataThreads[i].blocks[j].pos, dataThreads[i].blocks[j].size);
+//---creo los hilos------------
+	for(threadIndex=0;threadIndex<nodeCounter;++threadIndex){
+		pthread_create(&threads[threadIndex],NULL,(void*) runWorkerThread, (void*) &dataThreads[threadIndex]);
 	}
-*/
-//---------------
+//---joineo los hilos----
 
-//---cierro los hilos----
-
-	for (threadIndex=0;threadIndex<=(nodeCounter-1);++threadIndex){
+	for(threadIndex=0;threadIndex<nodeCounter;++threadIndex){
+		printf("Espero al thread %d\n", threadIndex);
 		pthread_join(threads[threadIndex],NULL);
 	}
-
+/*
+	*/
 //------libero memoria-----------------
-	usleep(99999);
 	int var=0;
 	for (var = 0; var < nodeCounter; ++var) {
 		free(dataThreads[var].blocks);
@@ -124,14 +109,12 @@ int transformFile(tr_datos yamaAnswer[], int totalRecords){
 	return EXIT_SUCCESS;
 }
 
-
 ///////////MAIN PROGRAM///////////
 
 int main(int argc, char* argv[]){
+	//recibo respuesta de Yama
 	int answerSize = sizeof(tr_answer)/sizeof(tr_answer[0]);
-
-	pthread_mutex_init(&mutex,NULL);
-	//validateArgs(argc, argv);					//valido argumentos
+	validateArgs(argc, argv);					//valido argumentos
 	transformFile(tr_answer,answerSize);		//se conecta a YAMA y devuelve array de struct
 	//runLocalReduction(argv[2]);				//ordena ejecución de Reductor Local
 	//runGlobalReduction						//ordena ejecución de Reductor Global
