73d9de335559eadcfe1c443b0eca3a5d176e2478
Adriangnr <adriancaslagnr@gmail.com>
2017-10-14 15:44:25 -0300

    protocolos

---
+++ tplibraries/tplibraries/protocol/filesystem_datanode.h
@@ -0,0 +1,19 @@
+/*
+ * filesystem_datanode.h
+ *
+ *  Created on: 14/10/2017
+ *      Author: utnso
+ */
+
+#ifndef TPLIBRARIES_PROTOCOL_FILESYSTEM_DATANODE_H_
+#define TPLIBRARIES_PROTOCOL_FILESYSTEM_DATANODE_H_
+
+
+
+typedef struct {
+    int idNodo;
+    int idBloque;
+    char* contenido
+} t_escribirDataNode;
+
+#endif /* TPLIBRARIES_PROTOCOL_FILESYSTEM_DATANODE_H_ */
---
+++ tplibraries/tplibraries/protocol/master_worker.h
@@ -0,0 +1,26 @@
+/*
+ * master_worker.h
+ *
+ *  Created on: 11/09/2017
+ *      Author: utnso
+ */
+
+#ifndef MASTER_WORKER_H_
+#define MASTER_WORKER_H_
+
+/*========TRANSFORM==========*/
+//----Request----//
+typedef struct{
+	int pos;
+	int size;
+}block;
+
+typedef struct{
+	char		code;
+	size_t		fileSize;
+	char*		file;
+	size_t		blocksSize;
+	block*		blocks;
+}tr_node;
+
+#endif /* MASTER_WORKER_H_ */
