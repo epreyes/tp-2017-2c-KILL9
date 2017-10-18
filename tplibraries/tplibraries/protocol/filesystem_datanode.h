ea7e5f5b73db254f706a6afec373763ffca79f76
miguel-t <tomicha.miguel@gmail.com>
2017-10-17 21:09:17 -0300

    agrego protocolo dataNode-fileSystem

--- tplibraries/tplibraries/protocol/filesystem_datanode.h
+++ tplibraries/tplibraries/protocol/filesystem_datanode.h
@@ -8,18 +8,29 @@
 #ifndef TPLIBRARIES_PROTOCOL_FILESYSTEM_DATANODE_H_
 #define TPLIBRARIES_PROTOCOL_FILESYSTEM_DATANODE_H_
 
+typedef struct __attribute__((packed)){
+	int idOperacion;
+	int idBloque;
+    int finByte;
+	char* contenido;
+} t_escribirBloque;
+
+typedef struct __attribute__((packed)){
+	int idOperacion;
+	int idBloque;
+    int finByte;
+	char* contenido;
+} t_leerBloque;
 
 
-typedef struct {
-    int idNodo;
-    int idBloque;
-    char* contenido;
-} t_escribirDataNode;
-
-
-#define	SUCCESS	    1
-#define GET_BLOQUE  2
+#define	SUCCESS	    	1
+#define GET_BLOQUE  	2
+#define SET_BLOQUE  	3
 #define ACCESOAPROBADO  3
 #define ACCESODENEGADO  1
+
+#define NODE_HSK 		1		//Envio DataNode
+#define HSK_OK   		1		//Respuesta FS
+#define SET_BLOQUE_OK 	1
 
 #endif /* TPLIBRARIES_PROTOCOL_FILESYSTEM_DATANODE_H_ */
